3875037c2ebccdeb2ecbb5005059ef02496f2ef0
Gleb Gulin <glepsgulin@gmail.com>
2019-09-11 22:02:40 +0300

    first

---
+++ WebRules/Controllers/RestfullyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebRules.Models.Rules;
+
+namespace WebRules.Controllers
+{
+    public class RestfullyController : Controller
+    {
+        public string Index()
+        {
+            Parser parser = new Parser();
+            parser.LoadRules();
+            string res = parser.result;
+            //Rule r = new Rule();
+
+            return res;
+        }
+    }
+}
---
+++ WebRules/Parser.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebRules.Models.Rules;
+
+namespace WebRules
+{
+    public class Parser
+    {
+        public Parser()
+        {
+            string _result = result;
+        }
+        private readonly string jsonpath = "rules.json";
+        public string result;
+        public void LoadRules()
+        {
+            try
+            {
+                string jsonconfiguration;
+                using (var reader = new StreamReader(jsonpath))
+                {
+                    jsonconfiguration = reader.ReadToEnd();
+                }
+                Rule r = JsonConvert.DeserializeObject<Rule>(jsonconfiguration);
+                r.rules.ToArray();
+               
+               
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+
+        }
+
+    }
+    
+}
